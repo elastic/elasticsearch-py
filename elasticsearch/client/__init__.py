48ec1ab4bbc0b49ac5dfcdd39fb341d93c7f4538
Honza Kral <honza.kral@gmail.com>
2013-07-10 14:07:33 +0200

    create & get API

--- elasticsearch/client/__init__.py
+++ elasticsearch/client/__init__.py
@@ -1,6 +1,7 @@
 from ..transport import Transport
 from .indices import IndicesClient
 from .cluster import ClusterClient
+from .utils import query_params, _make_path
 
 def _normalize_hosts(hosts):
     """
@@ -53,3 +54,51 @@ class Elasticsearch(object):
         # namespaced clients for compatibility with API names
         self.indices = IndicesClient(self)
         self.cluster = ClusterClient(self)
+
+    @query_params('consistency', 'id', 'parent', 'percolate', 'refresh', 'replication', 'routing', 'timeout', 'timestamp', 'ttl', 'version', 'version_type')
+    def create(self, index, doc_type, body, id=None, params=None):
+        """
+        The index API adds or updates a typed JSON document in a specific index, making it searchable.
+        http://elasticsearch.org/guide/reference/api/index_/
+
+        :arg index: The name of the index
+        :arg doc_type: The type of the document
+        :arg id: Document ID
+        :arg body: The document
+        :arg consistency: Explicit write consistency setting for the operation
+        :arg id: Specific document ID (when the POST method is used)
+        :arg parent: ID of the parent document
+        :arg percolate: Percolator queries to execute while indexing the document
+        :arg refresh: Refresh the index after performing the operation
+        :arg replication: Specific replication type, default u'sync'
+        :arg routing: Specific routing value
+        :arg timeout: Explicit operation timeout
+        :arg timestamp: Explicit timestamp for the document
+        :arg ttl: Expiration time for the document
+        :arg version: Explicit version number for concurrency control
+        :arg version_type: Specific version type
+        """
+        params['op_type'] = 'create'
+        status, data = self.transport.perform_request('PUT' if id else 'POST', _make_path(index, doc_type, id), params=params, body=body)
+        return data
+
+    @query_params('fields', 'parent', 'preference', 'realtime', 'refresh', 'routing')
+    def get(self, index, doc_type, id, params=None):
+        """
+        The get API allows to get a typed JSON document from the index based on its id.
+        http://elasticsearch.org/guide/reference/api/get/
+
+        :arg index: The name of the index
+        :arg doc_type: The type of the document (use `_all` to fetch the first document matching the ID across all types)
+        :arg id: The document ID
+        :arg fields: A comma-separated list of fields to return in the response
+        :arg parent: The ID of the parent document
+        :arg preference: Specify the node or shard the operation should be performed on (default: random)
+        :arg realtime: Specify whether to perform the operation in realtime or search mode
+        :arg refresh: Refresh the shard containing the document before performing the operation
+        :arg routing: Specific routing value
+        """
+        status, data = self.transport.perform_request('GET', _make_path(index, doc_type, id), params=params)
+        return data
+
+
