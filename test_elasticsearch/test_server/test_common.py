982ce508bef8f124eb00462232b75e91df70acfc
Honza Kral <honza.kral@gmail.com>
2013-07-15 14:43:58 +0200

    handle dots in yaml test dirs' names

--- test_elasticsearch/test_server/test_common.py
+++ test_elasticsearch/test_server/test_common.py
@@ -193,7 +193,7 @@ if yaml_dir:
             if not filename.endswith('.yaml'):
                 continue
             # ... parse them
-            name = 'Test' + ''.join(s.title() for s in path[len(yaml_dir) + 1:].split('/')) + filename.rsplit('.', 1)[0].title().replace('_', '')
+            name = ('Test' + ''.join(s.title() for s in path[len(yaml_dir) + 1:].split('/')) + filename.rsplit('.', 1)[0].title()).replace('_', '').replace('.', '')
             # and insert them into locals for test runner to find them
             locals()[name] = construct_case(join(path, filename), name)
 
