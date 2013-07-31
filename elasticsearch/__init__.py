3679018d80d32428a361cfd2c936239ebd535c0e
Honza Kral <honza.kral@gmail.com>
2013-07-31 17:38:52 +0200

    Keep version info in elasticsearch/__init__.py as well as setup

--- elasticsearch/__init__.py
+++ elasticsearch/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import
 
+VERSION = (0, 0, 1)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
 from elasticsearch.client import Elasticsearch
 from elasticsearch.transport import Transport
 from elasticsearch.connection_pool import ConnectionPool, ConnectionSelector, \
