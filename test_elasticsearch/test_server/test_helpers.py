4dc1b603c7b521dda3d196c093fd23b5a121cbb0
Richard Boulton <richard@tartarus.org>
2013-11-20 11:45:44 +0000

    Add a streaming bulk index helper function.

--- test_elasticsearch/test_server/test_helpers.py
+++ test_elasticsearch/test_server/test_helpers.py
@@ -77,6 +77,120 @@ class TestBulkIndex(ElasticTestCase):
         self.assertEquals(1, failed)
 
 
+class TestStreamingBulkIndex(ElasticTestCase):
+    def test_all_documents_get_inserted(self):
+        commands = [
+            [
+                {"index": {"_id": x}},
+                {'answer': x}
+            ]
+            for x in range(101)
+        ]
+        for x, (ok, cmd, result) in enumerate(helpers.streaming_bulk_index(
+            self.client, commands,
+            chunk_size=10,
+            index='test-index', doc_type='answers', refresh=True,
+        )):
+            self.assertTrue(ok)
+            self.assertEquals(cmd, [{"index": {"_id": x}}, {'answer': x}])
+            self.assertEquals(result, {
+                "index": {"_index": "test-index",
+                          "_type": "answers",
+                          "_id": str(x),
+                          "_version": 1,
+                          "ok": True, }})
+        self.assertEquals(x, 100)
+
+    def test_responses_are_reported_correctly(self):
+        self.client.indices.create("i", {
+            "mappings": {"t": {"properties": {"a": {"type": "integer"}}}},
+            "settings": {"number_of_shards": 1, "number_of_replicas": 0}
+        })
+        self.client.cluster.health(wait_for_status="yellow")
+
+        commands = [
+            [{"index": {}}, {'a': 42}],
+            [{"index": {'_id': 42}}, {"a": "c"}],
+        ]
+        results = tuple(helpers.streaming_bulk_index(
+            self.client, commands, index="i", doc_type="t"
+        ))
+        self.assertEqual(len(results), 2)
+        autoid = results[0][2].values()[0].get('_id')
+        self.assertEqual(results[0], (
+            True,
+            commands[0],
+            {u'create': {u'_id': autoid,
+                         u'_index': u'i',
+                         u'_type': u't',
+                         u'_version': 1,
+                         u'ok': True}}
+        ))
+
+        autoerrmsg = results[1][2].values()[0].get('error')
+        self.assertTrue('MapperParsingException' in autoerrmsg)
+        self.assertEqual(results[1], (
+            False,
+            commands[1],
+            {u'index': {u'_id': u'42',
+                        u'_index': u'i',
+                        u'_type': u't',
+                        u'error': autoerrmsg}}
+        ))
+
+    def test_update_using_command_object(self):
+        self.client.indices.create("i", {
+            "mappings": {"t": {"properties": {"a": {"type": "integer"}}}},
+            "settings": {"number_of_shards": 1, "number_of_replicas": 0}
+        })
+        self.client.cluster.health(wait_for_status="yellow")
+
+        commands = [
+            helpers.BulkCommand({"index": {}}, {'a': 42}, ext="hi"),
+            helpers.BulkCommand({"index": {'_id': 42}}, {"a": "c"}, ext="ho"),
+            helpers.BulkCommand({"delete": {'_id': 42}}, ext="hum"),
+        ]
+        results = tuple(helpers.streaming_bulk_index(
+            self.client, commands, index="i", doc_type="t"
+        ))
+        self.assertEqual(len(results), 3)
+        autoid = results[0][2].values()[0].get('_id')
+        self.assertEqual(results[0], (
+            True,
+            commands[0],
+            {u'create': {u'_id': autoid,
+                         u'_index': u'i',
+                         u'_type': u't',
+                         u'_version': 1,
+                         u'ok': True}}
+        ))
+        self.assertEqual(results[0][1].ext, 'hi')
+
+        autoerrmsg = results[1][2].values()[0].get('error')
+        self.assertTrue('MapperParsingException' in autoerrmsg)
+        self.assertEqual(results[1], (
+            False,
+            commands[1],
+            {u'index': {u'_id': u'42',
+                        u'_index': u'i',
+                        u'_type': u't',
+                        u'error': autoerrmsg}}
+        ))
+        self.assertEqual(results[1][1].ext, 'ho')
+
+        self.assertEqual(results[2], (
+            True,
+            commands[2],
+            {u'delete': {u'_id': u'42',
+                         u'_index': u'i',
+                         u'_type': u't',
+                         u'_version': 1,
+                         u'found': False,
+                         u'ok': True}}
+        ))
+        self.assertEqual(results[2][1].ext, 'hum')
+
+
 class TestScan(ElasticTestCase):
     def test_all_documents_are_read(self):
         bulk = []
