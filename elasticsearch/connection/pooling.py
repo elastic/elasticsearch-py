8a4c9900658b566becf6b7b9df0705bc13074f03
Michael Schier <schiermike@gmail.com>
2014-09-23 01:13:04 +1200

    addressed maintainer comments

--- elasticsearch/connection/pooling.py
+++ elasticsearch/connection/pooling.py
@@ -1,14 +1,20 @@
-from Queue import Queue
+try:
+    import queue
+except ImportError:
+    import Queue as queue
 from .base import Connection
 
 
 class PoolingConnection(Connection):
     def __init__(self, *args, **kwargs):
-        self._free_connections = Queue()
+        self._free_connections = queue.Queue()
         super(PoolingConnection, self).__init__(*args, **kwargs)
 
     def _get_connection(self):
-        return self._make_connection() if self._free_connections.empty() else self._free_connections.get()
+        try:
+            return self._free_connections.get_nowait()
+        except queue.Empty:
+            return self._make_connection()
 
     def _release_connection(self, con):
         self._free_connections.put(con)
