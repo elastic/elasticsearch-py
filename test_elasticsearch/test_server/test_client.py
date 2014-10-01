45a4d36ae1fb76dc8a240c85d6f2bfd022f1d636
Honza Král <honza.kral@gmail.com>
2014-10-01 15:52:46 +0200

    Added a failing unicode test

---
+++ test_elasticsearch/test_server/test_client.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from . import ElasticsearchTestCase
+
+class TestUnicode(ElasticsearchTestCase):
+    def test_indices_analyze(self):
+        self.client.indices.analyze(body='привет')
