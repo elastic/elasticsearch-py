4f632f7334af9e73490da23dfb32d0fe6327efa5
Julian Mehnle <jmehnle@agari.com>
2014-12-23 23:17:11 +0000

    Avoid generating 'No handlers could be found for logger "elasticsearch"' errors by installing no-op logging handler per <https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library>.

--- elasticsearch/__init__.py
+++ elasticsearch/__init__.py
@@ -4,6 +4,8 @@ VERSION = (1, 2, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
+import logging
+
 from .client import Elasticsearch
 from .transport import Transport
 from .connection_pool import ConnectionPool, ConnectionSelector, \
@@ -12,4 +14,9 @@ from .serializer import JSONSerializer
 from .connection import Connection, RequestsHttpConnection, \
     Urllib3HttpConnection, MemcachedConnection, ThriftConnection
 from .exceptions import *
+
+logger = logging.getLogger('elasticsearch')
+logger.addHandler(logging.NullHandler())
+    # Install no-op handler per
+    # <https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library>
 
