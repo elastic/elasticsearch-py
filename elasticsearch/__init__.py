b3c2ef2996e7b9635bd74112f651c7e45b9919f0
Honza Král <honza.kral@gmail.com>
2015-01-03 01:50:46 +0100

    version bump for dev

--- elasticsearch/__init__.py
+++ elasticsearch/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-VERSION = (1, 3, 0)
+VERSION = (1, 4, 0, 'dev')
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
@@ -9,8 +9,7 @@ import sys
 if (2, 7) <= sys.version_info < (3, 2):
     # On Python 2.7 and Python3 < 3.2, install no-op handler to silence
     # `No handlers could be found for logger "elasticsearch"` message per
-    # <https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library>,
-    # <https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library>:
+    # <https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library>
     import logging
     logger = logging.getLogger('elasticsearch')
     logger.addHandler(logging.NullHandler())
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import sys
 import os
 
-VERSION = (1, 3, 0)
+VERSION = (1, 4, 0, 'dev')
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
