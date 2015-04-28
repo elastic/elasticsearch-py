elastic/elasticsearch-py

commit 482f166c1af960aa23da61cb1308d66f40b89ed1

set propagate=False on elasticsearch.trace only if logger not already configured

elasticsearch/connection/base.py
@@ -7,8 +7,13 @@ except ImportError:
 from ..exceptions import TransportError, HTTP_EXCEPTIONS
 
 logger = logging.getLogger('elasticsearch')
+
+# create the elasticsearch.trace logger, but only set propagate to False if the
+# logger hasn't already been configured
+_tracer_already_configured = 'elasticsearch.trace' in logging.Logger.manager.loggerDict
 tracer = logging.getLogger('elasticsearch.trace')
-tracer.propagate = False
+tracer.propagate = not _tracer_already_configured
+
 
 class Connection(object):
     """

commit 032a1aa799e6a1ac1caa5844982021229927217c

Fixes issue introduced in 482f166 where the tracer spams when unconfigured.
In the aforementioned commit, the existing behavior was changed,
I believe in advertantly, to set propagate to False if the
trace logger already has been configured. This seems to be the
opposite of the internet, and this change restores what appears
to have been the intent, and silences the tracer unless
it has been configured, in which case it will propagate.

elasticsearch/connection/base.py
@@ -12,7 +12,8 @@ logger = logging.getLogger('elasticsearch')
 # logger hasn't already been configured
 _tracer_already_configured = 'elasticsearch.trace' in logging.Logger.manager.loggerDict
 tracer = logging.getLogger('elasticsearch.trace')
-tracer.propagate = not _tracer_already_configured
+if not _tracer_already_configured:
+    tracer.propagate = False
 
 
 class Connection(object):