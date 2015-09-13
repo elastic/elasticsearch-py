b1cb09ba8f9f9489c2785e2f3d7eb10b8e7a5ad1
Honza Král <honza.kral@gmail.com>
2015-09-13 21:06:22 +0200

    Better docstring for search_shards

--- elasticsearch/client/__init__.py
+++ elasticsearch/client/__init__.py
@@ -536,8 +536,10 @@ class Elasticsearch(object):
         out issues or planning optimizations with routing and shard preferences.
         `<http://www.elastic.co/guide/en/elasticsearch/reference/current/search-shards.html>`_
 
-        :arg index: The name of the index
-        :arg doc_type: The type of the document
+        :arg index: A comma-separated list of index names to search; use `_all`
+            or empty string to perform the operation on all indices
+        :arg doc_type: A comma-separated list of document types to search; leave
+            empty to perform the operation on all types
         :arg allow_no_indices: Whether to ignore if a wildcard indices
             expression resolves into no concrete indices. (This includes `_all`
             string or when no indices have been specified)
