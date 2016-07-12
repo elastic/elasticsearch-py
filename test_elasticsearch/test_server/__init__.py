ac7477803739d303df8374f916748173da32cb07
Honza Král <honza.kral@gmail.com>
2016-07-12 18:02:46 +0200

    Allow test client to be created with kwargs

--- test_elasticsearch/test_server/__init__.py
+++ test_elasticsearch/test_server/__init__.py
@@ -2,20 +2,23 @@ from elasticsearch.helpers.test import get_test_client, ElasticsearchTestCase as
 
 client = None
 
-def get_client():
+def get_client(**kwargs):
     global client
-    if client is not None:
+    if client is not None and not kwargs:
         return client
 
     # try and locate manual override in the local environment
     try:
         from test_elasticsearch.local import get_client as local_get_client
-        client = local_get_client()
+        new_client = local_get_client(**kwargs)
     except ImportError:
         # fallback to using vanilla client
-        client = get_test_client()
+        new_client = get_test_client(**kwargs)
 
-    return client
+    if not kwargs:
+        client = new_client
+
+    return new_client
 
 
 def setup():
@@ -23,5 +26,5 @@ def setup():
 
 class ElasticsearchTestCase(BaseTestCase):
     @staticmethod
-    def _get_client():
-        return get_client()
+    def _get_client(**kwargs):
+        return get_client(**kwargs)
