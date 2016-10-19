9c2af0f77c85e267ecbe53196cc75a45f5068702
Honza Král <honza.kral@gmail.com>
2016-10-19 16:33:14 +0200

    Version bump for development version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from setuptools import setup, find_packages
 import sys
 
-VERSION = (5, 0, 0)
+VERSION = (5, 1, 0, 'dev')
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
