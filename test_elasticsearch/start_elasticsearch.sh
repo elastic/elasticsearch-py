94ebf854048d1e19782038983e9ca31829a22947
Nick Lang <nick@nicklang.com>
2018-02-03 12:20:26 -0800

    better bash script to allow start from anywhere. closes #721

--- test_elasticsearch/start_elasticsearch.sh
+++ test_elasticsearch/start_elasticsearch.sh
@@ -5,12 +5,14 @@
 ES_VERSION=${ES_VERSION:-"6.1.3"}
 ES_TEST_SERVER=${ES_TEST_SERVER:-"http://localhost:9200"}
 
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 exec docker run -d \
     -e path.repo=/tmp \
     -e "repositories.url.allowed_urls=http://*" \
     -e node.attr.testattr=test \
     -e ES_HOST=$ES_TEST_SERVER \
-    -v `pwd`/../elasticsearch:/code/elasticsearch \
+    -v $SOURCE_DIR/../elasticsearch:/code/elasticsearch \
     -v /tmp:/tmp \
     -p "9200:9200" \
 docker.elastic.co/elasticsearch/elasticsearch-oss:$ES_VERSION
