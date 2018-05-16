b540a58f604dbe5fc969205916980af98b8214bf
Hugo <hugovk@users.noreply.github.com>
2018-05-16 17:00:24 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         where='.',
         exclude=('test_elasticsearch*', )
     ),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
