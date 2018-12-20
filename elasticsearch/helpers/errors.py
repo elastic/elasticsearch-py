1a5391403dce54ced0ef760a729177f2228a5c04
Asher Mancinelli <ashermancinelli@gmail.com>
2018-12-20 13:39:08 -0800

    PEP8 formatting & package structuring

---
+++ elasticsearch/helpers/errors.py
@@ -0,0 +1,16 @@
+
+
+from ..exceptions import ElasticsearchException
+
+
+class BulkIndexError(ElasticsearchException):
+    @property
+    def errors(self):
+        """ List of errors from execution of the last chunk. """
+        return self.args[1]
+
+
+class ScanError(ElasticsearchException):
+    def __init__(self, scroll_id, *args, **kwargs):
+        super(ScanError, self).__init__(*args, **kwargs)
+        self.scroll_id = scroll_id
