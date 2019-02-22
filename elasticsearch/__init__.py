2c6e60380a705965066fc9f6e81cfa83d71c4390
Brad <brad.solomon.1124@gmail.com>
2019-02-21 19:19:19 -0500

    Add NullHandler to logger for all Python versions (#896)

--- elasticsearch/__init__.py
+++ elasticsearch/__init__.py
@@ -1,18 +1,22 @@
+#flake8: noqa
 from __future__ import absolute_import
 
 VERSION = (6, 3, 1)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
 import sys
 
-if (2, 7) <= sys.version_info < (3, 2):
-    # On Python 2.7 and Python3 < 3.2, install no-op handler to silence
-    # `No handlers could be found for logger "elasticsearch"` message per
-    # <https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library>
-    import logging
-    logger = logging.getLogger('elasticsearch')
-    logger.addHandler(logging.NullHandler())
+logger = logging.getLogger('elasticsearch')
+logger.addHandler(logging.NullHandler())
 
 from .client import Elasticsearch
 from .transport import Transport
