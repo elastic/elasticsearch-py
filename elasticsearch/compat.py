4a0498ed7f19a07a412462e793ea24cce2964c67
Chris Bennight <chris@slowcar.net>
2019-11-05 20:51:38 -0500

    Proper import in Python2.x

--- elasticsearch/compat.py
+++ elasticsearch/compat.py
@@ -4,7 +4,7 @@ PY2 = sys.version_info[0] == 2
 
 if PY2:
     string_types = (basestring,)
-    from urllib import quote_plus, urlencode, unquote
+    from urllib import quote_plus, quote, urlencode, unquote
     from urlparse import urlparse
     from itertools import imap as map
     from Queue import Queue
