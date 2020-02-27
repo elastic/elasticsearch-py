ca160fc87229a0ee5d604c96facdfba48918d9bc
Seth Michael Larson <seth.larson@elastic.co>
2020-02-27 16:45:02 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"Elasticsearch"
-copyright = u"%d, Honza Král" % datetime.date.today().year
+copyright = u"%d, Elasticsearch B.V" % datetime.date.today().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
