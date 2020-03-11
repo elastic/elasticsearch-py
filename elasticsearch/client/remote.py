353afc4c01c46eda3a8340078b21fe03f484db6c
Seth Michael Larson <seth.larson@elastic.co>
2020-03-11 13:56:34 -0500

    Also add headers to all APIs not generated

--- elasticsearch/client/remote.py
+++ elasticsearch/client/remote.py
@@ -3,8 +3,10 @@ from .utils import NamespacedClient, query_params
 
 class RemoteClient(NamespacedClient):
     @query_params()
-    def info(self, params=None):
+    def info(self, params=None, headers=None):
         """
         `<http://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-remote-info.html>`_
         """
-        return self.transport.perform_request("GET", "/_remote/info", params=params)
+        return self.transport.perform_request(
+            "GET", "/_remote/info", params=params, headers=headers
+        )
