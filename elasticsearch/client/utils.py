16fb5edbf24ef1c484aada2b931f13d68ca87af1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-04-02 15:56:26 +0200

    Allow bytestring for bulk body
    With this commit we allow users to pass bytestrings for bulk bodies.
    This was already allowed previously but the client attempted to ensure a
    trailing newline which failed for bytestrings.
    
    Closes #919

--- elasticsearch/client/utils.py
+++ elasticsearch/client/utils.py
@@ -102,7 +102,9 @@ def _bulk_body(serializer, body):
         body = "\n".join(map(serializer.dumps, body))
 
     # bulk body must end with a newline
-    if not body.endswith("\n"):
+    if isinstance(body, bytes) and not body.endswith(b"\n"):
+        body += b"\n"
+    elif isinstance(body, str) and not body.endswith("\n"):
         body += "\n"
 
     return body
