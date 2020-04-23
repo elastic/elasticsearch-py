7554be775afbe3cf3ee467054b94a0d810b8582a
Seth Michael Larson <seth.larson@elastic.co>
2020-04-23 14:42:25 -0500

    Add license headers

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
 from os.path import join, dirname
 from setuptools import setup, find_packages
-import sys
 
 VERSION = (8, 0, 0)
 __version__ = VERSION
