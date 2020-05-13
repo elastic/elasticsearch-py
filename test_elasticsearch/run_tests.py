dbf955ed5571c13303c3f398b8d8741b3c4aed66
Seth Michael Larson <seth.larson@elastic.co>
2020-05-13 13:20:15 -0500

    Add AIOHttpConnection

--- test_elasticsearch/run_tests.py
+++ test_elasticsearch/run_tests.py
@@ -79,8 +79,12 @@ def run_all(argv=None):
             "--log-level=DEBUG",
             "--cache-clear",
             "-vv",
-            abspath(dirname(__file__)),
         ]
+
+        if sys.version_info < (3, 6):
+            argv.append("--ignore=test_elasticsearch/test_async/")
+
+        argv.append(abspath(dirname(__file__)),)
 
     exit_code = 0
     try:
