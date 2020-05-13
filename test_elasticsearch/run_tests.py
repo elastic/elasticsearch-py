8a8f6b07ea5678c8ef29e341fe81060f5dcb1717
Seth Michael Larson <seth.larson@elastic.co>
2020-05-13 13:20:15 -0500

    [7.8] Add AIOHttpConnection

--- test_elasticsearch/run_tests.py
+++ test_elasticsearch/run_tests.py
@@ -5,12 +5,11 @@
 
 from __future__ import print_function
 
+import atexit
 import sys
 from os import environ
 from os.path import dirname, join, pardir, abspath, exists
 import subprocess
-
-import nose
 
 
 def fetch_es_repo():
@@ -63,26 +62,37 @@ def fetch_es_repo():
 
 
 def run_all(argv=None):
-    sys.exitfunc = lambda: sys.stderr.write("Shutting down....\n")
+    atexit.register(lambda: sys.stderr.write("Shutting down....\n"))
 
     # fetch yaml tests
     fetch_es_repo()
 
     # always insert coverage when running tests
     if argv is None:
+        junit_xml = join(
+            abspath(dirname(dirname(__file__))), "junit", "elasticsearch-py-junit.xml"
+        )
         argv = [
-            "nosetests",
-            "--with-xunit",
-            "--with-xcoverage",
-            "--cover-package=elasticsearch",
-            "--cover-erase",
-            "--logging-filter=elasticsearch",
-            "--logging-level=DEBUG",
-            "--verbose",
-            "--with-id",
+            "pytest",
+            "--cov=elasticsearch",
+            "--junitxml=%s" % junit_xml,
+            "--log-level=DEBUG",
+            "--cache-clear",
+            "-vv",
         ]
 
-    nose.run_exit(argv=argv, defaultTest=abspath(dirname(__file__)))
+        # Python 3.6+ is required for async
+        if sys.version_info < (3, 6):
+            argv.append("--ignore=test_elasticsearch/test_async/")
+
+        argv.append(abspath(dirname(__file__)),)
+
+    exit_code = 0
+    try:
+        subprocess.check_call(argv, stdout=sys.stdout, stderr=sys.stderr)
+    except subprocess.CalledProcessError as e:
+        exit_code = e.returncode
+    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
