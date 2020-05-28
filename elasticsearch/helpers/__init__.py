db2a657a8b29ed973b2dd8a18fab40201b9da723
Seth Michael Larson <seth.larson@elastic.co>
2020-05-28 12:38:46 -0500

    Add async helpers

--- elasticsearch/helpers/__init__.py
+++ elasticsearch/helpers/__init__.py
@@ -2,6 +2,7 @@
 # Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 # See the LICENSE file in the project root for more information
 
+import sys
 from .errors import BulkIndexError, ScanError
 from .actions import expand_action, streaming_bulk, bulk, parallel_bulk
 from .actions import scan, reindex
@@ -19,3 +20,20 @@ __all__ = [
     "_chunk_actions",
     "_process_bulk_chunk",
 ]
+
+
+try:
+    # Asyncio only supported on Python 3.6+
+    if sys.version_info < (3, 6):
+        raise ImportError
+
+    from .._async.helpers import (
+        async_scan,
+        async_bulk,
+        async_reindex,
+        async_streaming_bulk,
+    )
+
+    __all__ += ["async_scan", "async_bulk", "async_reindex", "async_streaming_bulk"]
+except (ImportError, SyntaxError):
+    pass
