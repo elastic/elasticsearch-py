420e85a25a0b72dbc4975c78dc1dad4d7d2423d4
Nathan Meisels <nathan@forter.com>
2020-09-15 00:33:44 +0300

    Fix typo in ScanError

--- elasticsearch/helpers/actions.py
+++ elasticsearch/helpers/actions.py
@@ -527,7 +527,7 @@ def scan(
                 if raise_on_error:
                     raise ScanError(
                         scroll_id,
-                        "Scroll request has only succeeded on %d (+%d skiped) shards out of %d."
+                        "Scroll request has only succeeded on %d (+%d skipped) shards out of %d."
                         % (
                             resp["_shards"]["successful"],
                             resp["_shards"]["skipped"],
