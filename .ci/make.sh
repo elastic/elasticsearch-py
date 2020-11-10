4dbe5fcb3e5c667b1c6a2fc6849f9f603b495716
Seth Michael Larson <seth.larson@elastic.co>
2020-11-10 08:24:35 -0600

    Change 'make.sh release' to 'make.sh assemble'

--- .ci/make.sh
+++ .ci/make.sh
@@ -5,10 +5,12 @@ set -eo pipefail
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASE_DIR="$( dirname "$BASE_DIR" )"
 
-if [[ "$1" == "release" ]]; then
-  python $BASE_DIR/utils/build-dists.py $2
+if [[ "$1" == "assemble" ]]; then
   mkdir -p $BASE_DIR/.ci/output
-  cp $BASE_DIR/dist/* $BASE_DIR/.ci/output/
+  docker build . --tag elastic/elasticsearch-py -f .ci/Dockerfile
+  docker run --rm -it -v $BASE_DIR/.ci/output:/code/elasticsearch-py/dist \
+    elastic/elasticsearch-py \
+    python /code/elasticsearch-py/utils/build-dists.py $2
   exit 0
 fi
 
