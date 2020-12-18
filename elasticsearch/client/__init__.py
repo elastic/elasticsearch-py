4797356cf97b6f64f2e24286858c11bb58d17f16
Graywd <k.gray.jonescn@gmail.com>
2020-12-19 03:59:03 +0800

    [7.10] Remove duplicate IndicesClient in Elasticsearch constructor
    Co-authored-by: Seth Michael Larson <seth.larson@elastic.co>
    (cherry picked from commit e8cb3ca344424eb6c4f6703a00006ac8cfd24862)
    
    Co-authored-by: Graywd <k.gray.jonescn@gmail.com>

--- elasticsearch/client/__init__.py
+++ elasticsearch/client/__init__.py
@@ -223,7 +223,6 @@ class Elasticsearch(object):
         self.eql = EqlClient(self)
         self.graph = GraphClient(self)
         self.ilm = IlmClient(self)
-        self.indices = IndicesClient(self)
         self.license = LicenseClient(self)
         self.migration = MigrationClient(self)
         self.ml = MlClient(self)
