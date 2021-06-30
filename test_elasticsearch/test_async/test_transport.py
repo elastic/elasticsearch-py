6125d93aa3b45ded871984d8e42a43b397a4f7db
Seth Michael Larson <seth.larson@elastic.co>
2021-06-30 10:58:05 -0500

    [7.14] Verify we're connected to Elasticsearch before requests

--- test_elasticsearch/test_async/test_transport.py
+++ test_elasticsearch/test_async/test_transport.py
@@ -28,7 +28,14 @@ from mock import patch
 from elasticsearch import AsyncTransport
 from elasticsearch.connection import Connection
 from elasticsearch.connection_pool import DummyConnectionPool
-from elasticsearch.exceptions import ConnectionError, TransportError
+from elasticsearch.exceptions import (
+    AuthenticationException,
+    AuthorizationException,
+    ConnectionError,
+    ElasticsearchWarning,
+    NotElasticsearchError,
+    TransportError,
+)
 
 pytestmark = pytest.mark.asyncio
 
@@ -121,6 +128,7 @@ class TestTransport:
 
     async def test_request_timeout_extracted_from_params_and_passed(self):
         t = AsyncTransport([{}], connection_class=DummyConnection, meta_header=False)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", params={"request_timeout": 42})
         assert 1 == len(t.get_connection().calls)
@@ -135,6 +143,7 @@ class TestTransport:
         t = AsyncTransport(
             [{}], opaque_id="app-1", connection_class=DummyConnection, meta_header=False
         )
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/")
         assert 1 == len(t.get_connection().calls)
@@ -157,6 +166,7 @@ class TestTransport:
 
     async def test_request_with_custom_user_agent_header(self):
         t = AsyncTransport([{}], connection_class=DummyConnection, meta_header=False)
+        t._verified_elasticsearch = True
 
         await t.perform_request(
             "GET", "/", headers={"user-agent": "my-custom-value/1.2.3"}
@@ -172,6 +182,7 @@ class TestTransport:
         t = AsyncTransport(
             [{}], send_get_body_as="source", connection_class=DummyConnection
         )
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body={})
         assert 1 == len(t.get_connection().calls)
@@ -181,6 +192,7 @@ class TestTransport:
         t = AsyncTransport(
             [{}], send_get_body_as="POST", connection_class=DummyConnection
         )
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body={})
         assert 1 == len(t.get_connection().calls)
@@ -188,6 +200,7 @@ class TestTransport:
 
     async def test_client_meta_header(self):
         t = AsyncTransport([{}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body={})
         assert len(t.get_connection().calls) == 1
@@ -201,6 +214,7 @@ class TestTransport:
             HTTP_CLIENT_META = ("dm", "1.2.3")
 
         t = AsyncTransport([{}], connection_class=DummyConnectionWithMeta)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body={}, headers={"Custom": "header"})
         assert len(t.get_connection().calls) == 1
@@ -213,6 +227,7 @@ class TestTransport:
 
     async def test_client_meta_header_not_sent(self):
         t = AsyncTransport([{}], meta_header=False, connection_class=DummyConnection)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body={})
         assert len(t.get_connection().calls) == 1
@@ -221,6 +236,7 @@ class TestTransport:
 
     async def test_body_gets_encoded_into_bytes(self):
         t = AsyncTransport([{}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body="你好")
         assert 1 == len(t.get_connection().calls)
@@ -233,6 +249,7 @@ class TestTransport:
 
     async def test_body_bytes_get_passed_untouched(self):
         t = AsyncTransport([{}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
 
         body = b"\xe4\xbd\xa0\xe5\xa5\xbd"
         await t.perform_request("GET", "/", body=body)
@@ -241,6 +258,7 @@ class TestTransport:
 
     async def test_body_surrogates_replaced_encoded_into_bytes(self):
         t = AsyncTransport([{}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
 
         await t.perform_request("GET", "/", body="你好\uda6a")
         assert 1 == len(t.get_connection().calls)
@@ -253,6 +271,8 @@ class TestTransport:
 
     async def test_kwargs_passed_on_to_connections(self):
         t = AsyncTransport([{"host": "google.com"}], port=123)
+        t._verified_elasticsearch = True
+
         await t._async_call()
         assert 1 == len(t.connection_pool.connections)
         assert "http://google.com:123" == t.connection_pool.connections[0].host
@@ -260,6 +280,8 @@ class TestTransport:
     async def test_kwargs_passed_on_to_connection_pool(self):
         dt = object()
         t = AsyncTransport([{}, {}], dead_timeout=dt)
+        t._verified_elasticsearch = True
+
         await t._async_call()
         assert dt is t.connection_pool.dead_timeout
 
@@ -269,12 +291,15 @@ class TestTransport:
                 self.kwargs = kwargs
 
         t = AsyncTransport([{}], connection_class=MyConnection)
+        t._verified_elasticsearch = True
+
         await t._async_call()
         assert 1 == len(t.connection_pool.connections)
         assert isinstance(t.connection_pool.connections[0], MyConnection)
 
     def test_add_connection(self):
         t = AsyncTransport([{}], randomize_hosts=False)
+        t._verified_elasticsearch = True
         t.add_connection({"host": "google.com", "port": 1234})
 
         assert 2 == len(t.connection_pool.connections)
@@ -285,6 +310,7 @@ class TestTransport:
             [{"exception": ConnectionError("abandon ship")}],
             connection_class=DummyConnection,
         )
+        t._verified_elasticsearch = True
 
         connection_error = False
         try:
@@ -300,6 +326,7 @@ class TestTransport:
             [{"exception": ConnectionError("abandon ship")}] * 2,
             connection_class=DummyConnection,
         )
+        t._verified_elasticsearch = True
 
         connection_error = False
         try:
@@ -313,6 +340,8 @@ class TestTransport:
     async def test_resurrected_connection_will_be_marked_as_live_on_success(self):
         for method in ("GET", "HEAD"):
             t = AsyncTransport([{}, {}], connection_class=DummyConnection)
+            t._verified_elasticsearch = True
+
             await t._async_call()
             con1 = t.connection_pool.get_connection()
             con2 = t.connection_pool.get_connection()
@@ -338,6 +367,7 @@ class TestTransport:
             connection_class=DummyConnection,
             sniff_on_start=True,
         )
+
         await t._async_call()
         await t.sniffing_task  # Need to wait for the sniffing task to complete
 
@@ -351,6 +381,7 @@ class TestTransport:
             sniff_on_start=True,
             sniff_timeout=12,
         )
+
         await t._async_call()
         await t.sniffing_task  # Need to wait for the sniffing task to complete
 
@@ -393,6 +424,8 @@ class TestTransport:
             max_retries=0,
             randomize_hosts=False,
         )
+        t._verified_elasticsearch = True
+
         await t._async_call()
 
         connection_error = False
@@ -417,6 +450,7 @@ class TestTransport:
             max_retries=3,
             randomize_hosts=False,
         )
+        t._verified_elasticsearch = True
         await t._async_init()
 
         conn_err, conn_data = t.connection_pool.connections
@@ -432,6 +466,7 @@ class TestTransport:
             connection_class=DummyConnection,
             sniffer_timeout=5,
         )
+        t._verified_elasticsearch = True
         await t._async_call()
 
         for _ in range(4):
@@ -455,7 +490,9 @@ class TestTransport:
             connection_class=DummyConnection,
             sniff_timeout=42,
         )
+        t._verified_elasticsearch = True
         await t._async_call()
+
         await t.sniff_hosts()
         # Ensure we parsed out the fqdn and port from the fqdn/ip:port string.
         assert t.connection_pool.connection_opts[0][1] == {
@@ -472,6 +509,7 @@ class TestTransport:
             connection_class=DummyConnection,
             cloud_id="cluster:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQ0ZmE4ODIxZTc1NjM0MDMyYmVkMWNmMjIxMTBlMmY5NyQ0ZmE4ODIxZTc1NjM0MDMyYmVkMWNmMjIxMTBlMmY5Ng==",
         )
+        t._verified_elasticsearch = True
         await t._async_call()
 
         assert not t.sniff_on_connection_fail
@@ -482,6 +520,7 @@ class TestTransport:
 
     async def test_transport_close_closes_all_pool_connections(self):
         t = AsyncTransport([{}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
         await t._async_call()
 
         assert not any([conn.closed for conn in t.connection_pool.connections])
@@ -489,6 +528,7 @@ class TestTransport:
         assert all([conn.closed for conn in t.connection_pool.connections])
 
         t = AsyncTransport([{}, {}], connection_class=DummyConnection)
+        t._verified_elasticsearch = True
         await t._async_call()
 
         assert not any([conn.closed for conn in t.connection_pool.connections])
@@ -505,6 +545,7 @@ class TestTransport:
             connection_class=DummyConnection,
             sniff_on_start=True,
         )
+        t._verified_elasticsearch = True
 
         # If our initial sniffing attempt comes back
         # empty then we raise an error.
@@ -522,6 +563,7 @@ class TestTransport:
             connection_class=DummyConnection,
             sniff_on_start=True,
         )
+        t._verified_elasticsearch = True
 
         # Start the timer right before the first task
         # and have a bunch of tasks come in immediately.
@@ -556,6 +598,7 @@ class TestTransport:
             connection_class=DummyConnection,
             sniff_on_start=True,
         )
+        t._verified_elasticsearch = True
 
         # Start making _async_calls() before we cancel
         tasks = []
@@ -574,3 +617,209 @@ class TestTransport:
 
         # A lot quicker than 10 seconds defined in 'delay'
         assert duration < 1
+
+    @pytest.mark.parametrize(
+        ["headers", "data"],
+        [
+            (
+                {},
+                '{"version":{"number":"6.99.0"},"tagline":"You Know, for Search"}',
+            ),
+            (
+                {},
+                '{"version":{"number":"7.13.0","build_flavor":"default"},"tagline":"You Know, for Search"}',
+            ),
+            (
+                {"X-elastic-product": "Elasticsearch"},
+                '{"version":{"number":"7.14.0","build_flavor":"default"},"tagline":"You Know, for Search"}',
+            ),
+        ],
+    )
+    async def test_verify_elasticsearch(self, headers, data):
+        t = AsyncTransport(
+            [{"data": data, "headers": headers}], connection_class=DummyConnection
+        )
+        await t.perform_request("GET", "/_search")
+        assert t._verified_elasticsearch
+
+        calls = t.connection_pool.connections[0].calls
+        _ = [call[1]["headers"].pop("x-elastic-client-meta") for call in calls]
+
+        assert calls == [
+            (
+                ("GET", "/"),
+                {
+                    "headers": {
+                        "accept": "application/json",
+                    },
+                    "timeout": None,
+                },
+            ),
+            (
+                ("GET", "/_search", None, None),
+                {
+                    "headers": {},
+                    "ignore": (),
+                    "timeout": None,
+                },
+            ),
+        ]
+
+    @pytest.mark.parametrize(
+        "exception_cls", [AuthorizationException, AuthenticationException]
+    )
+    async def test_verify_elasticsearch_skips_on_auth_errors(self, exception_cls):
+        t = AsyncTransport(
+            [{"exception": exception_cls(exception_cls.status_code)}],
+            connection_class=DummyConnection,
+        )
+
+        with pytest.warns(ElasticsearchWarning) as warns:
+            with pytest.raises(exception_cls):
+                await t.perform_request(
+                    "GET", "/_search", headers={"Authorization": "testme"}
+                )
+
+        # Assert that a warning was raised due to security privileges
+        assert [str(w.message) for w in warns] == [
+            "The client is unable to verify that the server is "
+            "Elasticsearch due security privileges on the server side"
+        ]
+
+        # Assert that the cluster is "verified"
+        assert t._verified_elasticsearch
+
+        # See that the headers were passed along to the "info" request made
+        calls = t.connection_pool.connections[0].calls
+        _ = [call[1]["headers"].pop("x-elastic-client-meta") for call in calls]
+
+        assert calls == [
+            (
+                ("GET", "/"),
+                {
+                    "headers": {
+                        "accept": "application/json",
+                        "authorization": "testme",
+                    },
+                    "timeout": None,
+                },
+            ),
+            (
+                ("GET", "/_search", None, None),
+                {
+                    "headers": {
+                        "Authorization": "testme",
+                    },
+                    "ignore": (),
+                    "timeout": None,
+                },
+            ),
+        ]
+
+    async def test_multiple_requests_verify_elasticsearch_success(self, event_loop):
+        t = AsyncTransport(
+            [
+                {
+                    "data": '{"version":{"number":"7.13.0","build_flavor":"default"},"tagline":"You Know, for Search"}',
+                    "delay": 1,
+                }
+            ],
+            connection_class=DummyConnection,
+        )
+
+        results = []
+        completed_at = []
+
+        async def request_task():
+            try:
+                results.append(await t.perform_request("GET", "/_search"))
+            except Exception as e:
+                results.append(e)
+            completed_at.append(event_loop.time())
+
+        # Execute a bunch of requests concurrently.
+        tasks = []
+        start_time = event_loop.time()
+        for _ in range(10):
+            tasks.append(event_loop.create_task(request_task()))
+        await asyncio.gather(*tasks)
+        end_time = event_loop.time()
+
+        # Exactly 10 results completed
+        assert len(results) == 10
+
+        # No errors in the results
+        assert all(isinstance(result, dict) for result in results)
+
+        # Assert that this took longer than 2 seconds but less than 2.1 seconds
+        duration = end_time - start_time
+        assert 2 <= duration <= 2.1
+
+        # Assert that every result came after ~2 seconds, no fast completions.
+        assert all(
+            2 <= completed_time - start_time <= 2.1 for completed_time in completed_at
+        )
+
+        # Assert that the cluster is "verified"
+        assert t._verified_elasticsearch
+
+        # See that the first request is always 'GET /' for ES check
+        calls = t.connection_pool.connections[0].calls
+        assert calls[0][0] == ("GET", "/")
+
+        # The rest of the requests are 'GET /_search' afterwards
+        assert all(call[0][:2] == ("GET", "/_search") for call in calls[1:])
+
+    async def test_multiple_requests_verify_elasticsearch_errors(self, event_loop):
+        t = AsyncTransport(
+            [
+                {
+                    "data": '{"version":{"number":"7.13.0","build_flavor":"default"},"tagline":"BAD TAGLINE"}',
+                    "delay": 1,
+                }
+            ],
+            connection_class=DummyConnection,
+        )
+
+        results = []
+        completed_at = []
+
+        async def request_task():
+            try:
+                results.append(await t.perform_request("GET", "/_search"))
+            except Exception as e:
+                results.append(e)
+            completed_at.append(event_loop.time())
+
+        # Execute a bunch of requests concurrently.
+        tasks = []
+        start_time = event_loop.time()
+        for _ in range(10):
+            tasks.append(event_loop.create_task(request_task()))
+        await asyncio.gather(*tasks)
+        end_time = event_loop.time()
+
+        # Exactly 10 results completed
+        assert len(results) == 10
+
+        # All results were errors
+        assert all(isinstance(result, NotElasticsearchError) for result in results)
+
+        # Assert that one request was made but not 2 requests.
+        duration = end_time - start_time
+        assert 1 <= duration <= 1.1
+
+        # Assert that every result came after ~1 seconds, no fast completions.
+        assert all(
+            1 <= completed_time - start_time <= 1.1 for completed_time in completed_at
+        )
+
+        # Assert that the cluster is definitely not Elasticsearch
+        assert t._verified_elasticsearch is False
+
+        # See that the first request is always 'GET /' for ES check
+        calls = t.connection_pool.connections[0].calls
+        assert calls[0][0] == ("GET", "/")
+
+        # The rest of the requests are 'GET /_search' afterwards
+        assert all(call[0][:2] == ("GET", "/_search") for call in calls[1:])
