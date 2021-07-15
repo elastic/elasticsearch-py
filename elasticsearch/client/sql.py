887da1da0d66b5a867cfc2c0d3f6690cf6cf1133
Seth Michael Larson <seth.larson@elastic.co>
2021-07-15 15:29:27 -0500

    Update APIs to 7.14-SNAPSHOT

--- elasticsearch/client/sql.py
+++ elasticsearch/client/sql.py
@@ -15,7 +15,7 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
-from .utils import SKIP_IN_PATH, NamespacedClient, query_params
+from .utils import SKIP_IN_PATH, NamespacedClient, _make_path, query_params
 
 
 class SqlClient(NamespacedClient):
@@ -69,4 +69,67 @@ class SqlClient(NamespacedClient):
 
         return self.transport.perform_request(
             "POST", "/_sql/translate", params=params, headers=headers, body=body
+        )
+
+    @query_params()
+    def delete_async(self, id, params=None, headers=None):
+        """
+        Deletes an async SQL search or a stored synchronous SQL search. If the search
+        is still running, the API cancels it.
+
+        `<https://www.elastic.co/guide/en/elasticsearch/reference/7.14/delete-async-sql-search-api.html>`_
+
+        :arg id: The async search ID
+        """
+        if id in SKIP_IN_PATH:
+            raise ValueError("Empty value passed for a required argument 'id'.")
+
+        return self.transport.perform_request(
+            "DELETE",
+            _make_path("_sql", "async", "delete", id),
+            params=params,
+            headers=headers,
+        )
+
+    @query_params("delimiter", "format", "keep_alive", "wait_for_completion_timeout")
+    def get_async(self, id, params=None, headers=None):
+        """
+        Returns the current status and available results for an async SQL search or
+        stored synchronous SQL search
+
+        `<https://www.elastic.co/guide/en/elasticsearch/reference/7.14/get-async-sql-search-api.html>`_
+
+        :arg id: The async search ID
+        :arg delimiter: Separator for CSV results  Default: ,
+        :arg format: Short version of the Accept header, e.g. json, yaml
+        :arg keep_alive: Retention period for the search and its results
+            Default: 5d
+        :arg wait_for_completion_timeout: Duration to wait for complete
+            results
+        """
+        if id in SKIP_IN_PATH:
+            raise ValueError("Empty value passed for a required argument 'id'.")
+
+        return self.transport.perform_request(
+            "GET", _make_path("_sql", "async", id), params=params, headers=headers
+        )
+
+    @query_params()
+    def get_async_status(self, id, params=None, headers=None):
+        """
+        Returns the current status of an async SQL search or a stored synchronous SQL
+        search
+
+        `<https://www.elastic.co/guide/en/elasticsearch/reference/7.14/get-async-sql-search-status-api.html>`_
+
+        :arg id: The async search ID
+        """
+        if id in SKIP_IN_PATH:
+            raise ValueError("Empty value passed for a required argument 'id'.")
+
+        return self.transport.perform_request(
+            "GET",
+            _make_path("_sql", "async", "status", id),
+            params=params,
+            headers=headers,
         )
