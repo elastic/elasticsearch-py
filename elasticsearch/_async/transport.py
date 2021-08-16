af9515b05074458fb680746ec4dc70168a4a22b5
P. Sai Vinay <33659563+V1NAY8@users.noreply.github.com>
2021-08-17 00:06:59 +0530

    Add explicit parameters for AIOHttpConnection and AsyncTransport

--- elasticsearch/_async/transport.py
+++ elasticsearch/_async/transport.py
@@ -20,6 +20,7 @@ import logging
 import sys
 from itertools import chain
 
+from ..connection_pool import ConnectionPool
 from ..exceptions import (
     ConnectionError,
     ConnectionTimeout,
@@ -27,7 +28,8 @@ from ..exceptions import (
     TransportError,
     UnsupportedProductError,
 )
-from ..transport import Transport
+from ..serializer import JSONSerializer
+from ..transport import Transport, get_host_info
 from .compat import get_running_loop
 from .http_aiohttp import AIOHttpConnection
 
@@ -44,7 +46,26 @@ class AsyncTransport(Transport):
 
     DEFAULT_CONNECTION_CLASS = AIOHttpConnection
 
-    def __init__(self, hosts, *args, sniff_on_start=False, **kwargs):
+    def __init__(
+        self,
+        hosts,
+        connection_class=None,
+        connection_pool_class=ConnectionPool,
+        host_info_callback=get_host_info,
+        sniff_on_start=False,
+        sniffer_timeout=None,
+        sniff_timeout=0.1,
+        sniff_on_connection_fail=False,
+        serializer=JSONSerializer(),
+        serializers=None,
+        default_mimetype="application/json",
+        max_retries=3,
+        retry_on_status=(502, 503, 504),
+        retry_on_timeout=False,
+        send_get_body_as="GET",
+        meta_header=True,
+        **kwargs
+    ):
         """
         :arg hosts: list of dictionaries, each containing keyword arguments to
             create a `connection_class` instance
@@ -77,6 +98,8 @@ class AsyncTransport(Transport):
             don't support passing bodies with GET requests. If you set this to
             'POST' a POST method will be used instead, if to 'source' then the body
             will be serialized and passed as a query parameter `source`.
+        :arg meta_header: If True will send the 'X-Elastic-Client-Meta' HTTP header containing
+            simple client metadata. Setting to False will disable the header. Defaults to True.
 
         Any extra keyword arguments will be passed to the `connection_class`
         when creating and instance unless overridden by that connection's
@@ -88,7 +111,23 @@ class AsyncTransport(Transport):
         self._sniff_on_start_event = None  # type: asyncio.Event
 
         super(AsyncTransport, self).__init__(
-            *args, hosts=[], sniff_on_start=False, **kwargs
+            hosts=[],
+            connection_class=connection_class,
+            connection_pool_class=connection_pool_class,
+            host_info_callback=host_info_callback,
+            sniff_on_start=False,
+            sniffer_timeout=sniffer_timeout,
+            sniff_timeout=sniff_timeout,
+            sniff_on_connection_fail=sniff_on_connection_fail,
+            serializer=serializer,
+            serializers=serializers,
+            default_mimetype=default_mimetype,
+            max_retries=max_retries,
+            retry_on_status=retry_on_status,
+            retry_on_timeout=retry_on_timeout,
+            send_get_body_as=send_get_body_as,
+            meta_header=meta_header,
+            **kwargs,
         )
 
         # Don't enable sniffing on Cloud instances.
