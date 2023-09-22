1a70b2837ae8037771b772c5bbe5b51b536b2e6d
Josh Mock <joshua.mock@elastic.co>
2023-09-22 13:35:07 -0500

    Update vm.max_map_count in Buildkite jobs

--- .buildkite/run-elasticsearch.sh
+++ .buildkite/run-elasticsearch.sh
@@ -31,6 +31,10 @@ cleanup_node $es_node_name
 master_node_name=${es_node_name}
 cluster_name=${moniker}${suffix}
 
+if [[ "$BUILDKITE" == "true" ]]; then
+  sudo sysctl -w vm.max_map_count=262144
+fi
+
 declare -a volumes
 environment=($(cat <<-END
   --env ELASTIC_PASSWORD=$elastic_password
