6e37cea3638bd5c61b05e95366b2808a849c92c1
Quentin Pradet <quentin.pradet@elastic.co>
2023-10-30 14:39:33 +0400

    Show installed packages

--- noxfile.py
+++ noxfile.py
@@ -28,8 +28,8 @@ SOURCE_FILES = (
 
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
-    session.install(".")
-    session.install("-r", "dev-requirements.txt")
+    session.install(".", silent=False)
+    session.install("-r", "dev-requirements.txt", silent=False)
 
     session.run("python", "setup.py", "test")
 
