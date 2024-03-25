6328418e641f2f080258114caa7dcbdeefb34829
Iulia Feroli <iuliaferoli@gmail.com>
2024-03-25 15:19:08 +0100

    initial generation of examples

--- utils/generate-examples.py
+++ utils/generate-examples.py
@@ -106,6 +106,21 @@ files_to_generate = [
     "cluster/update-settings.asciidoc",
     "health/health.asciidoc",
     "cluster/reroute.asciidoc",
+    "ml/trained-models/apis/clear-trained-model-deployment-cache.asciidoc",
+    "ml/trained-models/apis/delete-trained-models-aliases.asciidoc",
+    "ml/trained-models/apis/delete-trained-models.asciidoc",
+    "ml/trained-models/apis/get-trained-models-stats.asciidoc",
+    "ml/trained-models/apis/get-trained-models.asciidoc",
+    "ml/trained-models/apis/infer-trained-model-deployment.asciidoc",
+    "ml/trained-models/apis/infer-trained-model.asciidoc",
+    "ml/trained-models/apis/put-trained-model-definition-part.asciidoc",
+    "ml/trained-models/apis/put-trained-model-vocabulary.asciidoc",
+    "ml/trained-models/apis/put-trained-models-aliases.asciidoc",
+    "ml/trained-models/apis/put-trained-models.asciidoc",
+    "ml/trained-models/apis/start-trained-model-deployment.asciidoc",
+    "ml/trained-models/apis/stop-trained-model-deployment.asciidoc",
+    "ml/trained-models/apis/update-trained-model-deployment.asciidoc",
+
 ]
 
 
