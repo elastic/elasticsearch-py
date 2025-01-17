4f22c2689d88aad8f1864753908980b67d83c416
Quentin Pradet <quentin.pradet@elastic.co>
2025-01-17 16:24:00 +0400

    Revert "Fix formatting of Markdown code blocks"
    This reverts commit 956175dcb48f896bbb49510d1220b911ea0835a6.

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -45,16 +45,6 @@ pygments_style = "sphinx"
 
 html_theme = "sphinx_rtd_theme"
 
-# These folders are copied to the documentation's HTML output
-html_static_path = ["_static"]
-
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = [
-    "css/custom.css",
-]
-
-
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "elastic-transport": (
