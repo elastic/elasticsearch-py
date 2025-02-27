e408b88fcc1def4d0c98bf67a37ca1648d4fe402
Quentin Pradet <quentin.pradet@elastic.co>
2025-02-27 14:46:55 +0400

    Run nox -rs format

--- test_elasticsearch/utils.py
+++ test_elasticsearch/utils.py
@@ -425,9 +425,9 @@ def is_xpack_template(name):
         return True
 
     return name in {
-	"agentless",
-	"agentless@mappings",
-	"agentless@settings",
+        "agentless",
+        "agentless@mappings",
+        "agentless@settings",
         "apm-10d@lifecycle",
         "apm-180d@lifecycle",
         "apm-390d@lifecycle",
