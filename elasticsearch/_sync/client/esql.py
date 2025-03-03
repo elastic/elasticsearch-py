7fa9a0faaeb6554f426dac202f5b0ddbccb82121
Quentin Pradet <quentin.pradet@elastic.co>
2025-03-03 17:25:57 +0400

    Fix lint

--- elasticsearch/_sync/client/esql.py
+++ elasticsearch/_sync/client/esql.py
@@ -20,7 +20,7 @@ import typing as t
 from elastic_transport import ObjectApiResponse
 
 from ._base import NamespacedClient
-from .utils import _rewrite_parameters
+from .utils import SKIP_IN_PATH, _quote, _rewrite_parameters
 
 
 class EsqlClient(NamespacedClient):
