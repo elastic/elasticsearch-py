58f1b37d5cae20cdb508df69fe8e9f618a42ad22
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-09-22 06:03:24 +0000

    Auto-generated API code

--- elasticsearch/_async/client/watcher.py
+++ elasticsearch/_async/client/watcher.py
@@ -552,11 +552,7 @@ class WatcherClient(NamespacedClient):
                 __body["transform"] = transform
             if trigger is not None:
                 __body["trigger"] = trigger
-        if not __body:
-            __body = None  # type: ignore[assignment]
-        __headers = {"accept": "application/json"}
-        if __body is not None:
-            __headers["content-type"] = "application/json"
+        __headers = {"accept": "application/json", "content-type": "application/json"}
         return await self.perform_request(  # type: ignore[return-value]
             "PUT",
             __path,
