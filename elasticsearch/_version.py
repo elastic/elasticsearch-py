a4e27316209f0aaca7c9bad87a050a4418fc2f13
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-10-24 16:43:47 +0100

    update schema version

--- elasticsearch/_version.py
+++ elasticsearch/_version.py
@@ -16,4 +16,4 @@
 #  under the License.
 
 __versionstr__ = "9.1.1"
-__es_specification_commit__ = "76b70737591e72220c94ac06d51c8d6c76ce7d78"
+__es_specification_commit__ = "cc623e3b52dd3dfd85848ee992713d37da020bfb"
