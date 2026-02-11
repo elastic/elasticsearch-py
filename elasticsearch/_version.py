72d018206d7cd18446de7ba5d649456317a14bd6
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-02-11 13:39:53 +0100

    Auto-generated code for main (#3320)
    * Auto-generated API code
    
    * adjust client for streams removal
    
    * do not run yaml tests that depend on feature flags
    
    * Remove one more reference to StreamsClient
    
    * Remove streams API from sphinx docs
    
    ---------
    
    Co-authored-by: Miguel Grinberg <miguel.grinberg@gmail.com>

--- elasticsearch/_version.py
+++ elasticsearch/_version.py
@@ -16,4 +16,4 @@
 #  under the License.
 
 __versionstr__ = "9.4.0"
-__es_specification_commit__ = "ae71fea58c94c0a027a0e03c69208c012d647846"
+__es_specification_commit__ = "56a97adc5f4990c06976811c9091ec6046894946"
