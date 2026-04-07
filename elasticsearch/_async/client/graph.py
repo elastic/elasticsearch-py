b4a26d9cae8da77121f4c31779406f490f2d9ebb
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-04-07 01:58:25 -0700

    Auto-generated API code (#3379)

--- elasticsearch/_async/client/graph.py
+++ elasticsearch/_async/client/graph.py
@@ -55,7 +55,7 @@ class GraphClient(NamespacedClient):
           You can exclude vertices that have already been returned.</p>
 
 
-        `<https://www.elastic.co/docs/api/doc/elasticsearch/v9/group/endpoint-graph>`_
+        `<https://www.elastic.co/docs/api/doc/elasticsearch/group/endpoint-graph>`_
 
         :param index: Name of the index.
         :param connections: Specifies or more fields from which you want to extract terms
