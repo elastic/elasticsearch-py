d416d12667c87e48e04511416121550f03e69642
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-08-31 15:02:12 +0000

    Auto-generated API code

--- elasticsearch/_version.py
+++ elasticsearch/_version.py
@@ -16,5 +16,5 @@
 #  under the License.
 
 __versionstr__ = "9.3.0"
-__es_specification_commit__ = "05ee9c5ed64ae8b6943c21eb88da78ad956c0f2e"
+__es_specification_commit__ = "71504006180fe7aeedad8864cc071690b8e34018"
 _SERVERLESS_API_VERSION = "2023-10-31"
